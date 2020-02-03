f76c645ba7a886cf66f99761d1bf8b3ba99c5305
Kamila Andrzejuk <kamilak680@gmail.com>
2020-02-03 21:03:24 +0100

    Test

--- src/com/Selenium_for_intellij/Main.java
+++ src/com/Selenium_for_intellij/Main.java
@@ -1,21 +1,45 @@
 package com.Selenium_for_intellij;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.Keys;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 
 public class Main
 {
-
     private WebDriver wd;
     private String url;
-    private String usernMail;
+    private String userMail;
     private String userPassword;
+    private String urlSubpage;
+    private String urlConversation;
+    private String randomMail;
+    private String randomText;
+
+    protected String getSaltString() 
+    {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 10) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+
+
+    }
 
     @Before
     public void setUp()
@@ -23,23 +47,52 @@ public class Main
         System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
         wd = new FirefoxDriver();
         url = "https://www.tidio.com/panel/login";
-        usernMail = "joannakorzen1@wp.pl";
+        urlSubpage = "https://widget-v4.tidiochat.com/preview.html?code=cpteevo4sxz2by18zulob7o7wgfscjtk";
+        urlConversation = "https://www.tidio.com/panel/conversations/";
+        userMail = "kamkaa24@wp.pl";
         userPassword = "Kamila1212";
+        randomMail = "@wp.pl";
+        randomText = "jfdsdfsd";
     }
 
     @Test
-    public void loginNegative()
+    public void loginPositive() throws InterruptedException 
     {
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(wd,10);
+        
         wd.get(url);
         WebElement loginName = wd.findElement(By.cssSelector("[placeholder='Email']"));
         loginName.clear();
-        loginName.sendKeys(usernMail);
+        loginName.sendKeys(userMail);
 
         WebElement passName = wd.findElement(By.cssSelector("[placeholder='Password']"));
         passName.clear();
         passName.sendKeys(userPassword);
 
-       wd.findElement(By.cssSelector("[stype='submit']" )).click();
+        wd.findElement(By.cssSelector("[type='submit']" )).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[class='ion-ios-chatbubble']")));
+        Assert.assertTrue(wd.getPageSource().contains("Squeeze the most value out of Tidio"));
+
+        wd.get(urlSubpage);
+        wd.switchTo().frame("tidio-chat-iframe");
+        wd.findElement(By.xpath("//button[@title='Yes, please!']")).click();
+        WebElement message = wd.findElement(By.cssSelector("[placeholder='Hit the buttons to respond']"));
+        wd.findElement(By.xpath("//button[@title='E-mail']")).click();
+        message.clear();
+        message.sendKeys(getSaltString()+randomMail);
+        Thread.sleep(5555);
+        message.sendKeys(Keys.ENTER);
+        Thread.sleep(5555);
+        message.sendKeys(randomText);
+        message.sendKeys(Keys.ENTER);
+        Thread.sleep(5555);
+        wd.findElement(By.cssSelector("[class='user-data-modal-submit']")).click();
+
+        wd.get(urlConversation);
+        wd.findElement(By.xpath("//*[contains(text(),'" + randomText + "')]"));
+        Assert.assertTrue((wd.getPageSource().contains(randomText)));
+        Thread.sleep(5555);
     }
 
     @After
@@ -47,10 +100,4 @@ public class Main
     {
         wd.quit();
     }
-
-
-
-
-
-
 }
